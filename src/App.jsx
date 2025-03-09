1b69fe54cb878c82ccefb6847ccce85508059177
Swornim Maharjan <imswornim@Swornims-MacBook-Pro.local>
2025-03-09 20:45:49 +1100

    Created Folders! :D CHEERSSSS

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,13 @@
 import "./App.css";
+import HomePage from "./Pages/HomePage";
 
 function App() {
-  return <>This is the best app in the world</>;
+  return (
+    <>
+      This is the best app in the world
+      <HomePage />
+    </>
+  );
 }
 
 export default App;
