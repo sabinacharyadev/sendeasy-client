ea3938ac33bcbc0b6717ea826c0b4e9e38b51875
Swornim Maharjan <imswornim@Swornims-MacBook-Pro.local>
2025-03-14 00:26:27 +1100

    Footer Added & Cover Pic Changed

---
+++ src/Components/Footer.jsx
@@ -0,0 +1,13 @@
+const Footer = () => {
+  return (
+    <div className="footer">
+      <p>© 2025 SendEasy. All rights reserved.</p>
+      <div className="footer-links">
+        <a href="/privacy">Privacy Policy</a> |{" "}
+        <a href="/terms">Terms of Service</a>
+      </div>
+    </div>
+  );
+};
+
+export default Footer;
